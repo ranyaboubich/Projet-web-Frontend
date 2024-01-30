1701707414a34ad26fb681dcface44ca62c67cbc
yavhaa <jaouher.jandoubi@insat.ucar.tn>
2024-01-30 04:42:51 +0100

    stars work but ugly

--- src/app/Details/discussion/stars/stars.component.ts
+++ src/app/Details/discussion/stars/stars.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
-  styleUrl: './stars.component.css'
+  styleUrl: './stars.component.css',
 })
 export class StarsComponent {
-
+  constructor() {}
+  ngOnInit(): void {
+    console.log('on init stars component');
+  }
 }
